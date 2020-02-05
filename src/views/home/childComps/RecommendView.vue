a5d9fb9152cd77b4beb655b8ec5d5bd67f1ac977
lzg999 <lzg940990149@outlook.com>
2020-02-05 15:30:50 +0800

    新增推荐模块

---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="recommend">
+    <div v-for="(item,index) in recommends" :key="index" class="recommendItem">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecommondView',
+  props: {
+    recommends: {
+      type: Array,
+      default(){
+        return []
+      }
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+    text-align: center;
+    padding: 6px 0 10px;
+    border-bottom: 8px solid #eee;
+  }
+  .recommendItem {
+    flex: 1;
+  }
+  .recommendItem img{
+    width: 80%;
+    margin-bottom: 5px;
+  }
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,34 +3,41 @@
     <nav-bar class="home-nav">
       <div slot="center">首页</div>
     </nav-bar>
+    <home-swiper :banners="banners" />
+    <Recommend-view :recommends="recommends"/>
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
-import {getHomeMultidata} from 'network/home'
+  import NavBar from 'components/common/navbar/NavBar'
+  import HomeSwiper from './childComps/HomeSwiper'
+  import RecommendView from './childComps/RecommendView'
 
-export default {
-  name: "home",
-  data() {
-    return {
-      banners: [],
-      recommends: []
+  import {getHomeMultidata} from 'network/home'
+
+  export default {
+    name: "home",
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView
+    },
+    methods: {
+
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
     }
-  },
-  components: {
-    NavBar
-  },
-  methods: {
-
-  },
-  created() {
-    getHomeMultidata().then(res => {
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    })
   }
-}
 </script>
 
 <style scoped>
