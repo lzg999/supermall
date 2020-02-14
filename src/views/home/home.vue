bc5f8a263a49fa47dd94f7c4dcb674bc71ffb0a1
lzg999 <lzg940990149@outlook.com>
2020-02-14 18:19:37 +0800

    home.js新增getHomeGoods方法，请求首页商品数据 home.vue封装请求实现复用

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -119,14 +119,19 @@
   import RecommendView from './childComps/RecommendView'
   import FeatureView from './childComps/FeatureView'
 
-  import {getHomeMultidata} from 'network/home'
+  import {getHomeMultidata, getHomeGoods} from 'network/home'
 
   export default {
     name: "home",
     data() {
       return {
         banners: [],
-        recommends: []
+        recommends: [],
+        goods: {
+          'pop':{page: 0, list: []},
+          'new':{page: 0, list: []},
+          'sell':{page: 0, list: []}
+        }
       }
     },
     components: {
@@ -137,13 +142,25 @@
       FeatureView
     },
     methods: {
-
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1;
+        getHomeGoods(type, page).then(res => {
+          this.goods[type].list.push(...res.data.list)
+          this.goods[type].page +=1
+        })
+      }
     },
     created() {
-      getHomeMultidata().then(res => {
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
-      })
+      this.getHomeMultidata(),
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
     }
   }
 </script>
