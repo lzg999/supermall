48d2185e9900a6a8fd7aa2a9c33d63b68a0624e8
lzg999 <lzg940990149@outlook.com>
2020-04-01 01:09:41 +0800

    商品详细页的实现

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ export function request(config){
     // 1、 创建Axios的实例
     const instance = axios.create({
       // baseURL: 'http://123.207.32.32:8000/api/wh',
-      baseURL: 'http://106.54.54.237:8000/api/wh',
+      baseURL: 'http://106.54.54.237:8000/api/h3',
       timeout: 5000
     })
     // 2、Axios的拦截器
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="detail">
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :top-imgs="topImgs"></detail-swiper>
-    <detail-base-info :goods="goods"></detail-base-info>
+    <detail-nav-bar class="detail-bar"></detail-nav-bar>
+    <Scroll class="scroll" ref="scroll">
+      <detail-swiper :top-imgs="topImgs"></detail-swiper>
+      <detail-base-info :goods="goods"></detail-base-info>
+      <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-goods-info :detail-info="detailInfo" @imgLoad="imgLoad"></detail-goods-info>
+      <detail-param-info :param-info="paramInfo"></detail-param-info>
+      <detail-comment-info :comment-info="commentInfo"></detail-comment-info>
+    </Scroll>
   </div>
 </template>
 
@@ -10,8 +16,14 @@
   import DetailNavBar from './childComps/DetailNavBar'
   import DetailSwiper from './childComps/DetailSwiper'
   import DetailBaseInfo from './childComps/DetailBaseInfo'
+  import DetailShopInfo from './childComps/DetailShopInfo'
+  import DetailGoodsInfo from './childComps/DetailGoodsInfo'
+  import DetailParamInfo from './childComps/DetailParamInfo'
+  import DetailCommentInfo from './childComps/DetailCommentInfo'
 
-  import {getDetail, Goods} from 'network/detail'
+  import Scroll from 'components/common/scroll/Scroll'
+
+  import {getDetail, Goods, Shop, GoodsParam} from 'network/detail'
 
 export default {
   name: 'Detail',
@@ -19,29 +31,73 @@ export default {
     return {
       iid: null,
       topImgs: [],
-      goods: {}
+      goods: {},
+      shop: {},
+      detailInfo: {},
+      paramInfo: {},
+      commentInfo: {}
     }
   },
   components: {
     DetailNavBar,
     DetailSwiper,
-    DetailBaseInfo
+    DetailBaseInfo,
+    DetailShopInfo,
+    DetailGoodsInfo,
+    DetailParamInfo,
+    DetailCommentInfo,
+    Scroll
   },
   created() {
     this.iid = this.$route.query.iid
     getDetail(this.iid).then(res => {
       // 2.1.获取结果
       const data = res.result;
+      console.log(data)
+
       // 2.2.获取顶部信息
       this.topImgs = data.itemInfo.topImages;
+
       // 2.3.获取商品信息
       this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services);
+
+      // 2.4.获取店铺信息
+      this.shop = new Shop(data.shopInfo);
+
+      // 2.5.获取商品详情信息
+      this.detailInfo = data.detailInfo;
+
+      // 2.6.获取参数信息
+      this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule);
+
+      // 2.7.获取评论信息
+      if (data.rate.list) {
+        this.commentInfo = data.rate.list[0];
+      }
     })
   },
-  
+  methods: {
+    imgLoad() {
+      this.$refs.scroll.refresh()
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+  #detail {
+    position: relative;
+    z-index: 11;
+    background-color: #fff;
+    height: 100vh;
+  }
+  .detail-bar {
+    position: relative;
+    top: 0;
+    background-color: #fff;
+    z-index: 9;
+  }
+  .scroll {
+    height: calc(100% - 44px)
+  }
 </style>
